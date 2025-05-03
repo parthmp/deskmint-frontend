90688b4582bf2d598a990036afe802ca267d4021
parthmp <parthparmaronline@gmail.com>
2025-05-03 07:08:01 +0530

    icons

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,8 +1,20 @@
 <template>
     <div>
         <div class="topbar bg-amber-300 flex flex-col md:flex-row">
-            <div class="w-64 md:w-64">123</div>
-            <div>123</div>
+            <div class="hidden w-64 md:w-64 md:block">123</div>
+            <div><menu-icon /></div>
         </div>
     </div>
 </template>
+
+<script lang="ts">
+    import MenuIcon from 'vue-material-design-icons/Menu.vue';
+
+    export default{
+        name: 'Dashboard',
+        components: {
+            MenuIcon
+        }
+    }
+
+</script>
