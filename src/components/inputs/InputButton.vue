57cb1b7b59d1eff413597d94cef657967578976e
parthmp <parthparmaronline@gmail.com>
2025-07-10 23:13:11 +0530

    day end

--- src/components/inputs/InputButton.vue
+++ src/components/inputs/InputButton.vue
@@ -4,7 +4,7 @@
  outline-none border-none transition-all duration-300 ease-linear relative z-0 
  bg-gradient-to-r from-deskmint-green-dark to-deskmint-green rounded-[5px] hover:after:w-full after:content-[''] after:absolute 
  after:z-[-1] after:transition-all after:duration-300 after:ease-linear after:right-0 
- after:top-0 after:w-0 after:h-full after:bg-deskmint-green-dark after:rounded-[5px] p-[15px] hover:shadow-lg w-full flex flex-col items-center"
+ after:top-0 after:w-0 after:h-full after:rounded-[5px] p-[15px] hover:shadow-lg w-full flex flex-col items-center"
 >
   Login
   <IconRotateClockwise class="animate-spin" :size="30"></IconRotateClockwise>
