4a160543e1ca436b9a85c5639b71329becd5ec74
parthmp <parthparmaronline@gmail.com>
2025-07-11 19:52:47 +0530

    checkbox changed

--- src/components/inputs/InputCheckboxOld.vue
+++ src/components/inputs/InputCheckboxOld.vue
@@ -1,16 +1,12 @@
 <template>
 	<span class="">
 		<span class="flex items-center gap-2">
-			<div class="inline-flex items-center">
-				<label class="flex items-center cursor-pointer relative">
-					<input type="checkbox" class="peer h-5 w-5 cursor-pointer transition-all border-1 border-input-border hover:border-input-border-hover appearance-none rounded shadow  checked:bg-deskmint-green-dark checked:bg-deskmint-green-dark" id="checkbox" v-model="input_value" @input="EmitModel" :class="{'border-red-500!':!is_valid && show_errors}" />
-					<span class="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none">
-					<svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor" stroke-width="1">
-						<path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
-					</svg>
-					</span>
+			<div class="checkbox-wrapper">
+				<label>
+					<input type="checkbox" v-model="input_value" id="checkbox" @input="EmitModel" />
+					<span class="checkbox" :class="{'border-red-500!':!is_valid && show_errors}"></span>
 				</label>
-			</div> 
+			</div>
 			<label for="checkbox">Remember me</label>
 		</span>
 	</span>
@@ -32,7 +28,7 @@
 
 	export default defineComponent({
 
-		name : 'InputCheckbox',
+		name : 'InputCheckboxOld',
 
 		props : {
 			modelValue : {
