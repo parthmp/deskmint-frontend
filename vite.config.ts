bcb5d582dcb9e49634990bc86e3d6da75022d15e
parthmp <parthparmaronline@gmail.com>
2025-09-24 19:27:22 +0530

    before color picker

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,15 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue(), tailwindcss(),
 ],
+// build: {
+//         rollupOptions: {
+//             output:{
+//                 manualChunks(id) {
+//                     if (id.includes('node_modules')) {
+//                         return id.toString().split('node_modules/')[1].split('/')[0].toString();
+//                     }
+//                 }
+//             }
+//         }
+//     }
 })
