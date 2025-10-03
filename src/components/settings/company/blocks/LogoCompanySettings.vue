e828fb21203be18057d6ecee24b8d3a73bcd4419
parthmp <parthparmaronline@gmail.com>
2025-10-04 02:52:56 +0530

    save company logo feature

--- src/components/settings/company/blocks/LogoCompanySettings.vue
+++ src/components/settings/company/blocks/LogoCompanySettings.vue
@@ -1,11 +1,16 @@
 <template>
 	<div>
-		<p>Note: only images (jpg, jpeg, png, gif, webp) are allowed to upload. Max file size must not exceed more than 5mb.</p>
-		<br>
-		<input-file label="Select logo image" icon="IconPhoto" :multiple="false" type="image" :mb_limit="5" ref="logo_image_file" @invalid_file="triggerInvalidFile" @invalid_size="triggerInvalidSize" @get_files="getFiles"></input-file>
+		<div v-if="data.loading">
+			<logo-company-settings-skeleton></logo-company-settings-skeleton>
+		</div>
+		<div v-if="!data.loading">
+			<p>Note: only images (jpg, jpeg, png, gif, webp) are allowed to upload. Max file size must not exceed more than 5mb.</p>
+			<br>
+			<input-file label="Select logo image" icon="IconPhoto" :multiple="false" type="image" :mb_limit="5" ref="logo_image_file" @invalid_file="triggerInvalidFile" @invalid_size="triggerInvalidSize" @get_files="getFiles"></input-file>
 
-		<input-button @click.prevent="saveLogoImage" label="Save" icon="IconCheck" :disabled="data.btn_disabled" class="lg:float-start"></input-button>
-		<div class="clear-both"></div>
+			<input-button @click.prevent="saveLogoImage" label="Save" icon="IconCheck" :disabled="data.btn_disabled" class="lg:float-start"></input-button>
+			<div class="clear-both"></div>
+		</div>
 	</div>
 </template>
 
@@ -14,6 +19,7 @@
 	import { reactive, ref } from 'vue';
 	import InputFile from '../../../inputs/InputFile.vue';
 	import { toastEvents } from '../../../../events/toastEvents';
+	import LogoCompanySettingsSkeleton from '../../../skeletons/LogoCompanySettingsSkeleton.vue';
 
 	import InputButton from '../../../inputs/InputButton.vue';
 	import api from '../../../../helpers/api';
@@ -23,12 +29,14 @@
 	}
 
 	interface LogoCompanySettingsInterface{
+		loading:boolean,
 		btn_disabled:boolean,
 		image : object
 	}
 
 	const logo_image_file = ref<LogoImageFileInterface | null>(null);
 	const data = reactive<LogoCompanySettingsInterface>({
+		loading:false,
 		btn_disabled : false,
 		image: {}
 	});
@@ -53,14 +61,26 @@
 	}
 
 	const saveLogoImage = () : void => {
-		
-		let form_data = new FormData();
-		form_data.append('logo', data.image)
-		api.post('manage-company-settings-logo', form_data).then(response => {
-			console.log(response);
-		}).catch(error => {
+		data.btn_disabled = true;
+		if(!data.image || !(data.image instanceof File)){
+			toastEvents.emit('toast', {
+				type: 'error',
+				message: 'Please select image file to upload'
+			});
+			data.btn_disabled = false;
+		}else{
+			let form_data = new FormData();
+			form_data.append('logo', data.image)
+			api.post('manage-company-settings-logo', form_data).then(response => {
+				
+			}).catch(error => {
 
-		});
+			}).finally(() => {
+				data.btn_disabled = false;
+			});
+		}
+
+		
 	}
 
 
