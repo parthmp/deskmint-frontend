7667342b4e2b906345f8ba8deeb4aa859b549d5b
parthmp <parthparmaronline@gmail.com>
2025-10-05 23:03:37 +0530

    additional company fields completed

--- src/components/settings/company/blocks/AdditionalFieldsCompanySettings.vue
+++ src/components/settings/company/blocks/AdditionalFieldsCompanySettings.vue
@@ -1,45 +1,68 @@
 <template>
-	<form @submit.prevent="addNewField">
-		<input-text label="Field label" v-model="data.field_label.value" :error="data.field_label.error" :required="true" ref="new_field_label"></input-text>
-		<div class="clear-both"></div>
-		<input-button label="Add" icon="IconPlus" class="lg:float-end"></input-button>
-		<div class="clear-both"></div>
-	</form>
+	<input-button label="Add new field" icon="IconPlus" class="lg:float-end" @click.prevent="data.add_new_field_toggle = !data.add_new_field_toggle"></input-button>
+	<div class="clear-both"></div>
 
-	<div v-for="(field,index) in data.all_fields" :key="field.key" class="mt-[20px] flex gap-5 items-center">
-		<input-text :label="field.label" v-model="field.value" :required="false" class="grow"></input-text>
-		<a href="javascript:;" class="text-red-500! block mt-[15px]" @click.prevent="deleteField(index)"><IconTrash></IconTrash></a>
-	</div>
-
-	<form @submit.prevent="saveFields">
-		<div v-if="data.all_fields.length > 0" class="mt-[25px]">
-			<input-button label="Save" icon="IconCheck" :disabled="data.btn_disabled" class="lg:float-end"></input-button>
+	<additional-fields-company-settings-skeleton v-if="data.loading"></additional-fields-company-settings-skeleton>
+	<div v-if="!data.loading">
+		<form v-if="data.add_new_field_toggle" @submit.prevent="addNewField">
+			<input-text label="Field label" v-model="data.field_label.value" :error="data.field_label.error" :required="true" ref="new_field_label"></input-text>
 			<div class="clear-both"></div>
-		</div>
-	</form>
+			<input-button label="Add" icon="IconPlus" class="lg:float-end"></input-button>
+			<div class="clear-both"></div>
+		</form>
+
+		<form @submit.prevent="saveFields">
+			<div v-for="(field,index) in data.all_fields" :key="field.key" class="mt-[20px] flex gap-5 items-center">
+				<input-text :label="field.label" v-model="field.value" :required="false" class="grow"></input-text>
+				<a href="javascript:;" class="text-red-500! block mt-[15px]" @click.prevent="deleteField(index)"><IconTrash></IconTrash></a>
+			</div>
+		
+			<div v-if="data.all_fields.length > 0" class="mt-[25px]">
+				<input-button label="Save" icon="IconCheck" :disabled="data.btn_disabled" class="lg:float-end"></input-button>
+				<div class="clear-both"></div>
+			</div>
+		</form>
+	</div>
 	
 </template>
 
 <script lang="ts" setup>
 
-	import { reactive, ref } from 'vue';
+	import { onMounted, reactive, ref } from 'vue';
 	import InputText from '../../../inputs/InputText.vue';
 	import InputButton from '../../../inputs/InputButton.vue';
 	import { toastEvents } from '../../../../events/toastEvents';
 
 	import { IconTrash } from '@tabler/icons-vue';
-import api from '../../../../helpers/api';
+	import api from '../../../../helpers/api';
 
-	const new_field_label = ref(null);
+	import AdditionalFieldsCompanySettingsSkeleton from '../../../skeletons/AdditionalFieldsCompanySettingsSkeleton.vue';
 
-	const data = reactive({
+	interface InputComponent{
+  		validate: () => boolean
+	}
+
+	interface AdditionalFieldsCompanySettingsInterface{
+		loading:boolean,
+		btn_disabled:boolean,
+		field_label:object,
+		all_fields:Array<object>,
+		field_key_count:number,
+		add_new_field_toggle:boolean
+	}
+
+	const new_field_label = ref<InputComponent | null>(null);
+
+	const data = reactive<AdditionalFieldsCompanySettingsInterface>({
+		loading : false,
 		btn_disabled : false,
 		field_label : {
 			value : '',
 			error: ''
 		},
 		all_fields : [],
-		field_key_count : 0
+		field_key_count : 0,
+		add_new_field_toggle: false
 	});
 
 	/* methods */
@@ -49,12 +72,12 @@ import api from '../../../../helpers/api';
 		
 		if(new_field_label.value.validate()){
 			
-			data.all_fields.push({
-					id : '',
-					key :data.field_key_count,
-					label : data.field_label.value,
-					value : ''
-				});
+			data.all_fields.unshift({
+				id : '',
+				key :data.field_key_count,
+				label : data.field_label.value,
+				value : ''
+			});
 
 			data.field_label.value = '';
 
@@ -68,18 +91,44 @@ import api from '../../../../helpers/api';
 	}
 
 	const deleteField = (index:number) : void => {
-		data.all_fields.splice(index, 1) ;
+
+		let temp = data.all_fields[index];
+
+		data.all_fields.splice(index, 1);
+
+		if(temp.id !== '' && temp.id !== null){
+			
+			api.delete('manage-company-settings-additional-fields', {
+				data : { id: temp.id }
+			}).then(response => {
+				
+			}).catch(error => {});
+		}
+		
 	}
 
 	const saveFields = () : void => {
-
+		data.btn_disabled = true;
 		api.post('manage-company-settings-additional-fields', {
 			all_fields : data.all_fields
 		}).then(response => {
-
-		}).catch(error => {});
+			fetchAllFields();
+		}).catch(error => {}).finally(() => { data.btn_disabled = false; });
 
 	}
+
+	const fetchAllFields = () : void => {
+		data.loading = true;
+		api.get('manage-company-settings-additional-fields').then(response => {
+			data.all_fields = response.data;
+			data.loading = false;
+		}).catch(error => {});
+	}
+
+	/* hooks */
+	onMounted(() => {
+		fetchAllFields();
+	})
 
 
 </script>
---
+++ src/components/skeletons/AdditionalFieldsCompanySettingsSkeleton.vue
@@ -0,0 +1,5 @@
+<template>
+	<div v-for="n in 8" :key="n" class="mt-[20px]">
+		<div class="h-[40px] animate-[pulse_1s_ease-in-out_infinite] bg-deskmint-loading rounded-lg w-full"></div>
+	</div>
+</template>
