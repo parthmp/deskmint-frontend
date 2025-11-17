8db08cb302ff46ac13ea0fc009bf3b9319858906
parthmp <parthparmaronline@gmail.com>
2025-11-17 19:41:23 +0530

    skeleton changes

--- src/components/skeletons/PaymentSettingsPaypalSeketon.vue
+++ src/components/skeletons/PaymentSettingsPaypalSeketon.vue
@@ -1,6 +1,9 @@
 <template>
 	<div class="h-[40px] animate-[pulse_1s_ease-in-out_infinite] bg-deskmint-loading rounded-lg w-full lg:w-[150px] lg:float-start mt-[25px]"></div>
+	<div class="h-[40px] animate-[pulse_1s_ease-in-out_infinite] bg-deskmint-loading rounded-lg w-full lg:w-[250px] lg:float-end mt-[25px]"></div>
 	<div class="clear-both"></div>
+	<br>
+	<div class="h-[30px] animate-[pulse_1s_ease-in-out_infinite] bg-deskmint-loading rounded-lg w-full"></div>
 	<br>
 	<div class="h-[20px] animate-[pulse_1s_ease-in-out_infinite] bg-deskmint-loading rounded-lg w-full"></div>
 	<br>
