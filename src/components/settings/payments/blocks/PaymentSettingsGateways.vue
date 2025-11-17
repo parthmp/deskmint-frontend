48328b66e01547da3e1731c6be9ccea6f52f814d
parthmp <parthparmaronline@gmail.com>
2025-11-17 18:59:24 +0530

    added status function for payment gateway setup

--- src/components/settings/payments/blocks/PaymentSettingsGateways.vue
+++ src/components/settings/payments/blocks/PaymentSettingsGateways.vue
@@ -1,8 +1,8 @@
 <template>
 	<div>
-
-		<div class="grid grid-cols-12 gap-5 items-stretch">
-				
+		<PaymentSettingsGatewaysSkeleton v-if="loading"></PaymentSettingsGatewaysSkeleton>
+		<div class="grid grid-cols-12 gap-5 items-stretch" v-if="!loading">
+			
 			<payment-gatway-block v-for="(gateway, index) in payment_gateways" :key="index" :image="gateway.image" :url="gateway.url" :active="gateway.active"></payment-gatway-block>
 			
 		</div>
@@ -14,19 +14,48 @@
 <script lang="ts" setup>
 
 	
+	import { onMounted, reactive, ref } from 'vue';
 	import PaymentGatwayBlock from '../../../UI/PaymentGatwayBlock.vue';
+	import api from '../../../../helpers/api';
 
-	const payment_gateways : Array<{ image : string, url: string, active : boolean }> = [
+	import PaymentSettingsGatewaysSkeleton from '../../../skeletons/PaymentSettingsGatewaysSkeleton.vue';
+	
+
+	const loading = ref<boolean>(true);
+
+	const payment_gateways : Array<{ image : string, url: string, active : boolean }> = reactive([
 		{
 			image : '../../.././src/assets/images/paypal_logo.png',
 			url: 'integrations/paypal',
-			active : true
+			active : false
 		},
 		{
 			image : '../../.././src/assets/images/stripe_logo.png',
 			url: 'integrations/stripe',
 			active : false
 		}
-	];
+	]);
+
+	const fetchPaymentGatewayStatus = async () : Promise<void> => {
+
+		try{
+
+			const response = await api.get('manage-payments-settings');
+			if(response.data.includes('paypal')){
+				payment_gateways[0].active = true;
+			}
+			if(response.data.includes('stripe')){
+				payment_gateways[1].active = true;
+			}
+
+		}finally{
+			loading.value = false;
+		}
+
+	}
+
+	onMounted(() : void => {
+		fetchPaymentGatewayStatus();
+	})
 
 </script>
---
+++ src/components/skeletons/PaymentSettingsGatewaysSkeleton.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="grid grid-cols-12 gap-5 items-stretch">
+	<div class="col-span-12 lg:col-span-3 bg-deskmint-cyan-light rounded-lg shadow-lg">
+		<div class="h-[310px] animate-[pulse_1s_ease-in-out_infinite] bg-deskmint-loading rounded-lg w-full">
+			
+		</div>
+		
+	</div>
+	<div class="col-span-12 lg:col-span-3 bg-deskmint-cyan-light rounded-lg shadow-lg">
+		<div class="h-[310px] animate-[pulse_1s_ease-in-out_infinite] bg-deskmint-loading rounded-lg w-full">
+			
+		</div>
+		
+	</div>
+	</div>
+</template>
