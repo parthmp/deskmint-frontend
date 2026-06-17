2862a3d765955644be6dc917c3171354972d8c47
parthmp <parthparmaronline@gmail.com>
2026-06-18 01:38:44 +0530

    sending invoice endpoint

--- src/components/invoices/Invoices.vue
+++ src/components/invoices/Invoices.vue
@@ -1,14 +1,48 @@
 <template>
-	<general-index-page page_title="Manage invoices" :enable_arranged_columns="true" base_url="manage-invoices" slug="invoices" :actions="['view','edit', 'delete', 'send invoice']" @action="handleAction"></general-index-page>
+	<general-index-page page_title="Manage invoices" :enable_arranged_columns="true" base_url="manage-invoices" slug="invoices" :actions="['view','edit', 'delete', 'download PDF', 'send invoice']" @action="handleAction"></general-index-page>
 </template>
 
 <script lang="ts" setup>
 	
 	/* using compositon API */
+	import { onMounted, reactive } from 'vue';
 	import GeneralIndexPage from '../blocks/GeneralIndexPage.vue';
+import api from '../../helpers/api.ts';
 
-	const handleAction = (obj:object) => {
-		//console.log(obj);
+	type actionObject = {
+		action:string,
+		row : {
+			company_id : number,
+			id : number
+		}
+	};
+
+	const data = reactive<{time_offset_minutes:number}>({
+		time_offset_minutes : 0
+	});
+
+	const handleAction = (obj:actionObject) => {
+		console.log(obj);
+		if(obj.action.toLowerCase() === 'send invoice'){
+			sendInvoice(obj.row.company_id, obj.row.id);
+		}
 	}
+
+	const sendInvoice = async (company_id : number, id : number) : Promise<void> => {
+
+		await api.get('manage-invoices/send-invoice', {
+			params : {
+				company_id : company_id,
+				invoice_id : id,
+				time_offset_minutes : data.time_offset_minutes
+			}
+		});
+
+	}
+
+	onMounted(() => {
+		const d = new Date();
+		data.time_offset_minutes = d.getTimezoneOffset();
+	});
 
 </script>
