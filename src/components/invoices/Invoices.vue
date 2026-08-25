b5aa9e3776beb6606d5db338340378dee316bc29
parthmp <parthparmaronline@gmail.com>
2026-08-25 11:01:48 +0530

    add credit + add payment for invoices

--- src/components/invoices/Invoices.vue
+++ src/components/invoices/Invoices.vue
@@ -1,16 +1,40 @@
 <template>
-	<general-index-page page_title="Manage invoices" :enable_arranged_columns="true" base_url="manage-invoices" slug="invoices" :actions="['view','edit', 'delete', cancel_data,'Add Payment', 'download PDF', 'send Invoice']" @action="handleAction" :checkbox_actions="['Delete', 'Export CSV']"></general-index-page>
+	<general-index-page page_title="Manage invoices" :enable_arranged_columns="true" base_url="manage-invoices" slug="invoices" :actions="['view','edit', 'delete', cancel_data,add_payment, add_credit,'download PDF', 'send Invoice']" @action="handleAction" :checkbox_actions="['Delete', 'Export CSV']"></general-index-page>
+	<Popup :header="data.popup_header" :show_popup="data.show_popup" :blocker="true" :scrollable="false" @closed="closePopup" :close_outside="true" >
+		<AddPaymentToInvoiceSkeleton v-if="data.loading"></AddPaymentToInvoiceSkeleton>
+		<form @submit.prevent="handleAddCreditOrPayment" v-if="!data.loading">
+			<div class="mt-2">
+				<p>Invoice : {{ data.selected_invoice_number }}</p>
+				<p>Due : {{ data.selected_invoice_due }} ({{ data.selected_invoice_currency }})</p>
+				<template v-if="data.apply_type === 'credit'">
+					<InputNumber step="0.01" min="0.01" label="Enter credit amount" v-model="data.apply_amount.value" :error="data.apply_amount.error" :required="true" ref="apply_amount_ref" placeholder="Enter credit amount"></InputNumber>
+				</template>
+				<template v-if="data.apply_type === 'payment'">
+					<InputNumber step="0.01" min="0.01" label="Enter payment amount" v-model="data.apply_amount.value" :error="data.apply_amount.error" :required="true" ref="apply_amount_ref" placeholder="Enter payment amount"></InputNumber>
+					<input-select label="Payment type" placeholder="Select payment type" :disabled="false" v-model="data.payment_type.value" :error="data.payment_type.error" :required="true" :options="data.payment_types" ref="payment_type_ref"></input-select>
+				</template>
+				<InputButton btn_text="Save" icon="IconCheck" :disabled="data.btn_disabled"></InputButton>
+			</div>
+		</form>
+	
+	</Popup>
 </template>
 
 <script lang="ts" setup>
 	
 /* using compositon API */
-import { onMounted, reactive } from 'vue';
+import { onMounted, reactive, ref, watch } from 'vue';
 import GeneralIndexPage from '../blocks/GeneralIndexPage.vue';
 import api from '../../helpers/api.ts';
 import { toastEvents } from '../../events/toastEvents.ts';
 import { useRouter } from 'vue-router';
 import common from '../../helpers/common.ts';
+import Popup from '../UI/Popup.vue';
+import InputNumber from '../inputs/InputNumber.vue';
+import InputButton from '../inputs/InputButton.vue';
+import Decimal from 'decimal.js';
+import InputSelect from '../inputs/InputSelect.vue';
+import AddPaymentToInvoiceSkeleton from '../skeletons/AddPaymentToInvoiceSkeleton.vue';
 
 const router = useRouter();
 
@@ -22,9 +46,71 @@ type actionObject = {
 	}
 };
 
-const data = reactive<{time_offset_minutes:number}>({
-	time_offset_minutes : 0
+interface InvoicesInterface {
+	time_offset_minutes : number,
+	show_popup : boolean,
+	popup_header : string,
+	apply_type: string,
+	selected_invoice_id : number,
+	selected_invoice_number : string,
+	selected_invoice_due : string,
+	selected_invoice_currency : string,
+	apply_amount : {
+		error: string,
+		value : string
+	},
+	btn_disabled: boolean,
+	loading: boolean,
+	payment_types : Array<object>
+	payment_type : {
+		error: string,
+		value : string
+	}
+}
+
+const data = reactive<InvoicesInterface>({
+	time_offset_minutes : 0,
+	show_popup : false,
+	popup_header : '',
+	apply_type : '',
+	selected_invoice_id : 0,
+	selected_invoice_number : '',
+	selected_invoice_due : '',
+	selected_invoice_currency : '',
+	apply_amount : {
+		error: 'Please enter amount',
+		value : ''
+	},
+	btn_disabled : false,
+	loading : false,
+	payment_types : [],
+	payment_type : {
+		error: 'Please select payment type',
+		value : ''
+	}
 });
+
+const payment_type_ref = ref();
+const apply_amount_ref = ref();
+
+let temp_obj = null;
+
+watch(() => data.payment_type.value, () => {
+	data.payment_type.error = '';
+	const payment_type_v = payment_type_ref.value.validate();
+	if(!payment_type_v){
+		data.payment_type.error = 'Please select payment type';
+	}
+});
+
+watch(() => data.apply_amount.value, () => {
+	data.apply_amount.error = '';
+	const apply_amount_v = apply_amount_ref.value.validate();
+	if(!apply_amount_v){
+		data.apply_amount.error = 'Please enter amount';
+	}
+});
+
 
 const cancel_data = {
 
@@ -36,10 +122,24 @@ const cancel_data = {
 
 };
 
+const add_payment = {
+	labels : {
+		add_payment : [1,2, 4]
+	},
+	mapped : 'status'
+};
 
+
+const add_credit = {
+	labels : {
+		add_credit : [1,2, 4]
+	},
+	mapped : 'status'
+};
 
 const handleAction = (obj:actionObject) => {
 	
+	temp_obj = obj;
 	if(obj.action.toLowerCase() === 'send invoice'){
 		if(obj.row.status.value == 1){
 			obj.row.status.value = 2;
@@ -72,9 +172,24 @@ const handleAction = (obj:actionObject) => {
 			obj.row.status.highlight = 'success';
 			toggleInvoiceCancel(obj.row.id, 2);
 		}
+	}else if(obj.action.toLowerCase() === 'add_credit'){
+		data.show_popup = true;
+		data.popup_header = 'Add Credit';
+		data.apply_type = 'credit';
+		data.selected_invoice_id = obj.row.id;
+		data.selected_invoice_number = obj.row.invoice_number;
+		data.selected_invoice_currency = obj.row.c_code;
+		data.selected_invoice_due = obj.row.balance_due;
 		
-		
-		
+	}else if(obj.action.toLowerCase() === 'add_payment'){
+		data.show_popup = true;
+		data.popup_header = 'Add Payment';
+		data.apply_type = 'payment';
+		data.selected_invoice_id = obj.row.id;
+		data.selected_invoice_number = obj.row.invoice_number;
+		data.selected_invoice_currency = obj.row.c_code;
+		data.selected_invoice_due = obj.row.balance_due;
+		fetchPaymentTypes();
 	}
 }
 
@@ -127,6 +242,75 @@ const downloadPDF = async (company_id : number, id : number) : Promise<void> => 
 	}catch(e){
 		
 	}
+}
+
+const closePopup = () : void => {
+	data.show_popup = false;
+	data.apply_amount.value = '';
+}
+
+const fetchPaymentTypes = async () => {
+	data.loading = true;
+	const response = await api.get(`manage-payment-requests/payment-types`);
+	data.payment_types = response.data;
+	data.loading = false;
+}
+
+const handleAddCreditOrPayment = async () : Promise<void> => {
+	
+	const apply_amount_v = apply_amount_ref.value.validate();
+
+	let payment_type_v = true;
+
+	if(data.apply_type === 'payment'){
+		payment_type_v = payment_type_ref.value.validate();
+	}
+	
+	if(!payment_type_v || !apply_amount_v){
+		return ;
+	}
+
+	const due = new Decimal(data.selected_invoice_due);
+	const applied_amount = new Decimal(data.apply_amount.value);
+
+	if(applied_amount.equals(new Decimal(0))){
+		return ;
+	}
+
+	if(applied_amount.greaterThan(due)){
+		toastEvents.emit('toast', {
+			type: 'error',
+			message: `You can not apply more than ${due.toFixed(2).toString()}`
+		});
+		return ;
+	}
+
+	try{
+
+		data.btn_disabled = true;
+		const response = await api.post(`manage-invoices/add-credit-or-payment`, {
+			invoice_id : data.selected_invoice_id,
+			amount : data.apply_amount.value,
+			payment_type : data.payment_type.value,
+			type : data.apply_type
+		});
+
+		const meta = response.data.meta;
+
+		temp_obj.row.status.value = meta.status;
+		temp_obj.row.status.text = meta.status_text;
+		temp_obj.row.status.highlight = meta.highlight;
+		temp_obj.row.balance_due = meta.balance_due;
+		closePopup();
+
+	}catch(e){
+		
+	}finally{
+		data.btn_disabled = false;	
+	}
+
+	
+
 }
 
 onMounted(() => {
---
+++ src/components/skeletons/AddPaymentToInvoiceSkeleton.vue
@@ -0,0 +1,8 @@
+<template>
+	<div class="h-[20px] animate-[pulse_1s_ease-in-out_infinite] bg-deskmint-loading rounded-lg w-full mt-[25px]"></div>
+	<div class="h-[20px] animate-[pulse_1s_ease-in-out_infinite] bg-deskmint-loading rounded-lg w-full mt-[25px]"></div>
+	<div class="h-[40px] animate-[pulse_1s_ease-in-out_infinite] bg-deskmint-loading rounded-lg w-full mt-[25px]"></div>
+	<div class="h-[40px] animate-[pulse_1s_ease-in-out_infinite] bg-deskmint-loading rounded-lg w-full mt-[25px]"></div>
+	<div class="h-[40px] animate-[pulse_1s_ease-in-out_infinite] bg-deskmint-loading rounded-lg w-full mt-[25px]"></div>
+	
+</template>
